3644e9754625a1bbab0d9bad4e4850b1ab7456c3
Jessica Nunes <nunesjessica44@outlook.com>
2021-09-12 13:43:25 -0300

    formulario POST e PHP

--- metodos/index.php
+++ metodos/index.php
@@ -9,17 +9,28 @@
     <title>metodos</title>
 </head>
     <body>
+        <form method="post">
+            <input type="text" placeholder="Número 1" name="num1" id="n1-index"/>
+            <input type="text" placeholder="Número 2" name="num2" id="n2-index"/>
+            <input type="submit" value="Somar"/>
+        </form>
+
         <?php
-            somar(8,2);
+            somar($_POST['num1'], $_POST['num2']);
+        ?>
+
+
+        <?php
+            /*somar(8,2);
             echo "</br>";
 
             echo "subtração = " . subtrair(8,2);   
             echo "</br>";
-            
+
             $total = multiplicar();  
             echo "multiplicação = " . $total;
 
-            dividir();
+            dividir();*/
         ?>
     </body>
 </html>
